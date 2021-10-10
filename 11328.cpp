8117416199513edf1a966fc061ddc3210801111e
Haruponea <92024463+Haruponea@users.noreply.github.com>
2021-10-10 17:52:31 +0900

    Create 11328.cpp

---
+++ 11328.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool check(string& s1, string& s2) {
+	int arr1[26] = {};
+	int arr2[26] = {};
+	for (char c : s1) {
+		arr1[c - 'a']++;
+	}
+	for (char c : s2) {
+		arr2[c - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		if (arr1[i] != arr2[i]) return false;
+	}
+	return true;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s1, s2;
+		cin >> s1 >> s2;
+		if (check(s1, s2)) cout << "Possible\n";
+		else cout << "Impossible\n";
+	}
+}
