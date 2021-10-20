3d790618702b4a41f2619a9690d426cbdf0e0ef3
Haruponea <92024463+Haruponea@users.noreply.github.com>
2021-10-20 22:35:02 +0900

    Create 15652.cpp

---
+++ 15652.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[9];
+void func(int level) {
+	if (level == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i]<<' ';
+		}
+		cout << '\n';
+		return;
+	}
+	int k = (level == 0) ? 1 : arr[level - 1];
+	for (int i = k; i <= n; i++) {
+		arr[level] = i;
+		func(level + 1);
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+	func(0);
+}
