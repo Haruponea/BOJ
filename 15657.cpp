aa5ff20dc0b340961dc2d6862e01f72ff6bffff9
Haruponea <92024463+Haruponea@users.noreply.github.com>
2021-10-21 14:35:43 +0900

    Create 15657.cpp

---
+++ 15657.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[9];
+int ans[9];
+void func(int level,int mx) {
+	if (level == m) {
+		for (int i =0;i<m;i++)
+			cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	int st = 0;
+	if (level != 0) st = mx;
+	for (int i = st; i < n; i++) {
+		ans[level] = arr[i];
+		func(level + 1, i);
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	func(0, 0);
+}
