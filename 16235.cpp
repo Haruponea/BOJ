54a098b60bd44c1b7c244fde7d50dd503259258f
Haruponea <92024463+Haruponea@users.noreply.github.com>
2021-11-03 18:07:21 +0900

    Update 16235.cpp

--- 16235.cpp
+++ 16235.cpp
@@ -2,17 +2,16 @@
 #include <algorithm>
 #include <deque>
 using namespace std;
-int dead[12][12];
 int ground[12][12];
 deque<int> tree[12][12];
 int A[12][12];
 int dx[8] = { -1,-1,-1,0,1,1,1,0 };
 int dy[8] = { -1,0,1,1,1,0,-1,-1 };
-int n, m, k;
+int n, m, K;
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n >> m >> k;
+    cin >> n >> m >> K;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++) {
             ground[i][j] = 5;
@@ -30,29 +29,30 @@ int main(void) {
             if (!tree[i][j].empty())
                 sort(tree[i][j].begin(),tree[i][j].end());
     int year = 0;
-    while (year < k) {
+    while (year < K) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!tree[i][j].empty()) {
-                    for (auto itr = tree[i][j].begin(); itr != tree[i][j].end();) {
-                        if (*itr > ground[i][j]) {
-                            dead[i][j] += *itr / 2;
-                            itr = tree[i][j].erase(itr);
+                    int k = 0;
+                    for (; k < tree[i][j].size(); k++) {
+                        if (tree[i][j][k] <= ground[i][j]) {
+                            ground[i][j] -= tree[i][j][k]++;
                         }
-                        else {
-                            ground[i][j] -= *itr;
-                            *itr += 1;
-                            itr++;
-                        }
+                        else break;
                     }
+                    int idx = k;
+                    for (; k < tree[i][j].size(); k++) {
+                        ground[i][j] += tree[i][j][k] / 2;
+                    }
+                    tree[i][j].erase(tree[i][j].begin() + idx, tree[i][j].end());
                 }
             }
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!tree[i][j].empty()) {
-                    for (auto itr = tree[i][j].begin(); itr != tree[i][j].end();itr++) {
-                        if (*itr % 5 == 0) {
+                    for (int k = 0; k < tree[i][j].size(); k++) {
+                        if (tree[i][j][k] % 5 == 0) {
                             for (int dir = 0; dir < 8; dir++) {
                                 int nx = i + dx[dir];
                                 int ny = j + dy[dir];
@@ -66,8 +66,7 @@ int main(void) {
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                ground[i][j] += A[i][j] + dead[i][j];
-                dead[i][j] = 0;
+                ground[i][j] += A[i][j];
             }
         }
         year++;
