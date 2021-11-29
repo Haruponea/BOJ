897fbf242df2b25693db30a68cf5038f2bfb2acd
Haruponea <92024463+Haruponea@users.noreply.github.com>
2021-11-29 11:41:22 +0900

    Create 11055.cpp

---
+++ 11055.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int dp[1003];
+int arr[1003];
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        dp[i] = arr[i];
+    }
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                dp[i] = max(dp[i], arr[i] + dp[j]);
+            }
+        }
+    }
+    cout << *max_element(dp + 1, dp + 1 + n);
+}
